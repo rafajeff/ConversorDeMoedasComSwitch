7c36da6ca740b460b42a8c51d4c9e764fe80cc12
rafael jeferson <rafajeferson@hotmail.com>
2019-09-25 13:00:02 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDeMoedasComSwitch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConversorDeMoedasComSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            decimal n1, n2, resultado;
+
+            Console.WriteLine("Digite o tipo de moeda que quer converter");
+            Console.WriteLine("\nR = Real");
+            Console.WriteLine("\nUSR = Dolar");
+            op =Console.ReadLine();
+            Console.WriteLine("Digite o valor da cotação");
+            n1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor a ser cotado");
+            n2 = decimal.Parse(Console.ReadLine());
+            
+            switch (op)
+
+            {
+                case "R":
+                    resultado = Somar.Soma(n1, n2);
+                    Console.WriteLine($"O resultado é:R${resultado:N2}");
+                    break;
+
+                case "USR":
+                    resultado = Somar.Soma(n1, n2);
+                    Console.WriteLine($"O resultado é:${resultado:N2}");
+                    break;
+
+                default:
+                    Console.WriteLine("Operaçao Inválida");
+                    break;
+
+            }
+                
+        }
+    }
+}
---
+++ ConversorDeMoedasComSwitch/Somar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorDeMoedasComSwitch
+{
+    static class Somar
+    {
+        public static decimal Soma(decimal n1, decimal n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
+
